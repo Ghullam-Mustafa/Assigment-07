b38dbeb7c8705d74293fa9f8c9af2eb2cff3de3f
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-03-30 20:59:41 +0500

    1. Write a js program to find maximum between two numbers.

---
+++ index.js
@@ -0,0 +1,7 @@
+// 1. Write a js program to find maximum between two numbers.
+{
+var firstValue = prompt("Enter first value");
+var secondValue = prompt("Enter second value");
+console.log(firstValue | secondValue);
+}
+
