a449f0a6985d80934a2268896245cc528f05c101
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-04-01 21:54:10 +0500

    Second

--- index.js
+++ index.js
@@ -17,9 +17,37 @@
 //     }
     
 // }
-// ---------------------------------------------------------------------------------------------------
 // 2. Write a js program to find maximum between three numbers.
+// {
+// var oneValue =+ prompt("Enter first value");
+// var twoValue =+ prompt("Enter second value");
+// var anyValue = ("secndValue > firstValue");
+// var threeValue =+ prompt("Enter third value");
+// if (twoValue > oneValue){
+// console.log(oneValue );
+// }
+// else if  ( threeValue > anyValue) {
+//  console.log(twoValue);
+// }
+// else {
+//     console.log(threeValue);
+// }
+// }
 
+function maximum(numb1 ,numb2 , numb3) {
+    if (numb1 > numb2 && numb1 > numb3) {
+        console.log(`Your Number1 "${numb1} "is Greater then NUmber2" ${numb2}" and Number3 
+        "${numb3}"`);
+    }
+    else if (numb2 > numb3 && numb2 > numb1) {
+        console.log(`${numb2} is Greater then ${numb1} and ${numb3}`);
+    }
+    else{
+        console.log(`${numb3} is Greater then ${numb1} and ${numb2}`);
+    }
+}
+numb1 = prompt("Enter first value");
+numb2 = prompt("Enter second value");
+numb3 = prompt("Enter third value");
 
-
-// --------------------------------------------------------------------------------
+maximum(numb1,numb2,numb3)
